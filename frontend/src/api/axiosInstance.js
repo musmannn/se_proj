e80fa78a52c577f428933574f2b8ef79751afe65
Muhammad Usman <muhammad.usman11914@gmail.com>
2026-05-08 13:44:14 +0500

    change api

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api'
+  baseURL: 'https://se-proj-icyf.vercel.app/'
 });
 
 api.interceptors.request.use((config) => {
